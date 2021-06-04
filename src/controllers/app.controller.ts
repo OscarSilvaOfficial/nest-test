b255e01f55ca7eef2ebcc11d7c5ccc3e1d99d381
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-06-04 20:45:13 -0300

    update

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HostParam, Param, Query, Redirect, Req } from '@nestjs/common';
 import { AppService } from '../services/app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() request: Request) {
+    return request.httpVersion
+  }
+
+  @Get('/google')
+  @Redirect('https://google.com')
+  redirectToGoogle() {
+    return 
+  }
+
+  @Get('/query')
+  getQuery(@Query() query) {
+    return query
+  }
+
+  @Get('params/:id')
+  findOne(@Param() params): string {
+    console.log(params)
+    return `This action returns a ${JSON.stringify(params)}`;
+  }
+
+  @Get('/host')
+  getInfo(@HostParam() account: string) {
+    return account;
   }
 }
